3613a816ad4a4c9ab090a43ac99d3ed073048e9f
Falk Hoppe <falkhoppe81@gmail.com>
2017-10-19 21:34:04 +0200

    fixed build

--- src/leitmotiv-cli.mjs
+++ src/leitmotiv-cli.mjs
@@ -1,7 +1,9 @@
 // @flow
 
+import chalk from 'chalk'
 import leitmotiv from './leitmotiv.mjs'
 
 export default (val: string) => {
+  console.log(chalk.blue(val))
   leitmotiv(val)
 }
--- src/leitmotiv.mjs
+++ src/leitmotiv.mjs
@@ -1,6 +1,5 @@
 // @flow
 
 export default (val: string): string => {
-  console.log(val)
   return val
 }
